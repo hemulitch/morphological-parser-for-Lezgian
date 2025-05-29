e5bcb1c2af39172bba70ca237f31284aeff2ce8e
аня <90281943+hemulitch@users.noreply.github.com>
2025-05-29 11:58:57 +0300

    Update make_table.py

--- cg_parser/make_table.py
+++ cg_parser/make_table.py
@@ -3,10 +3,6 @@ import subprocess
 import pandas as pd 
 
 def generate_forms(analysis,wordform):
-    """
-    generating segmented forms from an analysis using lez_generator_sep.hfstol.
-    returns generated form or "ERROR" is no forms can be generated.
-    """
     try:
         proc = subprocess.run(
             ['hfst-proc', '-q', 'lez_generator_sep.hfstol'],
@@ -28,34 +24,52 @@ def generate_forms(analysis,wordform):
         output = "ERROR"
     return output
 
-def parse_analysis(analysis):
-    """
-    making a table from disambiguated output of cg parser for further evaluation (accuracy)
-    returns table in format word,analysis,sep_form
-    """
-    table = {'word':[],'analysis':[],'sep_form':[]}
-    parts = analysis.split("$")
-    for part in parts:
-        part = part.replace("^", "").strip()
-        part = part.split("/")
-        wordform = part[0]
-        analysis = part[1:]
-        analysis_clean = []
-        segmented_forms = []
-        for ana in analysis:
-            if "*" in ana:
-                analysis_clean.append("")
-                segmented_forms.append("")
-            else:
-                ana = ana.replace("<>", "\\")
-                analysis_clean.append(ana.replace("\\",""))
-                segments = generate_forms(ana,wordform)
-                segmented_forms.append(segments)
-        table['word'].append(wordform)
-        table['analysis'].append("/".join(analysis_clean))
-        table['sep_form'].append("/".join(segmented_forms))
-    return table
+def split_analysis(analysis):
+    if "*" in analysis:
+        stem, pos, tags = "", "", ""
+    else:
+        analysis = analysis.split("<")
+        stem = analysis[0]
+        pos = analysis[1].replace("<","").replace(">","")
+        if len(analysis) == 2:
+            tags = ""
+        else:
+            tags = " ".join(analysis[2:]).replace("<","").replace(">","").replace(" ",",")
+    return stem, pos, tags
 
+def parse_analysis(analysis):
+    table = {'id':[],'word':[], 'predicted_stem':[], 'predicted_pos':[], 'predicted_tags':[], 'predicted_sep_form':[], 'unrecognized':[]}
+    parts = analysis.split("$")
+    counter = 0
+    for part in parts:
+        if '<punct>' not in part:
+            part = part.replace("^","").strip()
+            part = part.split("/")
+            wordform = part[0]
+            analysis = part[1:]
+            if len(analysis) == 1:
+                ana = analysis[0].replace("<>", "\\")
+                table["id"].append(counter)
+                table["word"].append(wordform)
+                table["predicted_sep_form"].append(generate_forms(ana,wordform))
+                stem, pos, tags = split_analysis(ana.replace("\\",""))
+                table["predicted_stem"].append(stem)
+                table["predicted_pos"].append(pos)
+                table["predicted_tags"].append(tags)
+                table["unrecognized"].append(0) # потом вручную заменяла тэги для тех токенов, которые не распознали
+            else:
+                for ana in analysis:
+                    ana = ana.replace("<>", "\\")
+                    table["id"].append(counter)
+                    table["word"].append(wordform)
+                    table["predicted_sep_form"].append(generate_forms(ana,wordform))
+                    stem, pos, tags = split_analysis(ana.replace("\\",""))
+                    table["predicted_stem"].append(stem)
+                    table["predicted_pos"].append(pos)
+                    table["predicted_tags"].append(tags)
+                    table["unrecognized"].append(1)
+            counter += 1
+    return(table)
         
 if __name__ == '__main__':
     with open(sys.argv[1], "r", encoding='utf-8') as f:
